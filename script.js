8033d9072a2f953402070faa9636c76ce54117cb
Kirill Makhnovskiy <script.makhnovskiy@gmail.com>
2022-08-18 17:37:12 +0300

    modified homework

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
+
 const firstNumber = Number(prompt('Choose First number'));
-const operation = prompt('Choose operation for: "+" "-" "/" "*"');
-const secondNumber = Number(prompt('Choose Second number'));
- if(Number.isNaN(firstNumber)){
-    alert('Wrong FIRST Number!');
-} else if(Number.isNaN(secondNumber)) {
-    alert('Wrong SECOND Number!');
-} else if(operation === '+'){
-    alert(`Your result is ${firstNumber + secondNumber}`);
-} else if(operation === '-') {
-    alert(`Your result is ${firstNumber - secondNumber}`);
-} else if(operation === '/') {
-    alert(`Your result is ${firstNumber / secondNumber}`);
-} else if(operation === '*') {
-    alert(`Your result is ${firstNumber * secondNumber}`);
-} else {
-    alert('This operation is not exist')
+if(Number.isNaN(firstNumber)) {
+    alert('wrong first number');
+}
+
+while (!Number.isNaN(firstNumber)) {
+    const operation = prompt('Choose operation for: "+" "-" "/" "*"');
+    if(operation !== '+' && operation !== '-' && operation !== '/' && operation !== '*') {
+        alert('This operation is not exist');
+        break;
+    }
+    const secondNumber = Number(prompt('Choose Second number'));
+    if(Number.isNaN(secondNumber)) {
+        alert('wrong second number');
+        break;
+    } else if(operation === '+'){
+        alert(`Your result is ${firstNumber + secondNumber}`);
+        break;
+    } else if(operation === '-') {
+        alert(`Your result is ${firstNumber - secondNumber}`);
+        break;
+    } else if(operation === '/') {
+        alert(`Your result is ${firstNumber / secondNumber}`);
+        break;
+    } else if(operation === "*") {
+        alert(`Your result is ${firstNumber * secondNumber}`);
+        break;
+    } 
 }
